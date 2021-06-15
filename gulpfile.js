70327b1b57193b02ac9149f8b074825dbd42cc52
SeniorDmitriy <dimaponomarenko443@gmail.com>
2021-06-15 22:54:33 +0300

    end adaptive, fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,7 @@ function images(){
 function build() {
   return src([
     'app/**/*.html',
-    'app/css/style.min.css',
+    'app/css/*.min.css',
     'app/js/main.min.js'
   ], {base: 'app'})
   .pipe(dest('dist'))
